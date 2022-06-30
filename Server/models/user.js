c72629155d1dde257dfc8d0c3269be4c931d4493
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-06-30 20:08:22 -0300

    DataBase UserSchema

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,27 +1,27 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      user.belongsToMany(thing,{through:'userThings'})
-    }
-  }
-  user.init({
-    email: DataTypes.STRING,
-    nickName: DataTypes.STRING,
-    password: DataTypes.STRING,
-    dateBorn: DataTypes.DATE,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
-  return user;
-};
+const {Schema, model, default: mongoose} = require('mongoose')
+const {isEmail} = require('validator')
+new Schema({
+  email: {
+    type: String,
+    require: true,
+    validate: [isEmail, 'invalid email'],
+    createIndexes: {unique: true}
+  },
+  nickName: 
+  {
+    type: String,
+    require:true,
+    createIndexes: {unique:true}
+  },
+  password: {
+    type: String, 
+    require:true
+  },
+  dateBorn: {
+    type: Date
+  },
+  things:[{
+    type: mongoose.Schema.Types,
+    ref: 'Thing' //referencia a things
+  }]
+})
